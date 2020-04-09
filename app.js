2df089809d9f509b6607244aa6cfb3532b813e17
annie0sc <annie0sc@gmail.com>
2020-04-09 15:09:19 -0500

    Made some changes to the link.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://dwstudent:<dw123>@cluster0-luj7k.azure.mongodb.net/local_library?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("local_library").collection("devices");
   // perform actions on the collection object
   client.close();
 });
